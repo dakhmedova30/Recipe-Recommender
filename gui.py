3ff75fc4936b999421e5af283e40d3e0eaac0b9d
herenali <herenalihoiching@gmail.com>
2024-03-22 14:17:33 -0400

    Updated recipes.py and requirements
    Added a function that returns the string representation of a recipe and modified create_radar_chart to save the radar chart as a png

--- gui.py
+++ gui.py
@@ -3,6 +3,7 @@ TODO: add description and copyright
 """
 import customtkinter as ctk
 from PIL import Image
+import recipes
 
 ctk.set_appearance_mode("dark")
 ctk.set_default_color_theme("dark-blue")
